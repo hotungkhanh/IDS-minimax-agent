ebded133a4c1a2d41901af34c2ee9cfa44ffd904
Eden Xu <130119691+FlyingPufferFish@users.noreply.github.com>
2024-04-30 04:48:41 +0000

    Begin minimax, debug inf loop

--- agent/helpers.py
+++ agent/helpers.py
@@ -12,33 +12,7 @@ from .part_a.core import PlayerColor, Coord, PlaceAction, BOARD_N
 from .part_a.program import State
 from .part_a.utils import render_board
 from queue import PriorityQueue as pq
-import time
-
-
+import time, math
 
 def test():   
     print("test")
-
-def minimax(state: State, depth: int, maximisingPlayer: bool):
-    ''' 
-    recursive pseudocode:
-
-    if depth == 0 or game over in state then:
-        return static eval of state
-
-    if maximisingPlayer then:
-        maxEval = -infinity
-        for each child of state:
-            eval = minimax(child, depth - 1, false)
-            maxEval = max(maxEval, eval)
-        return maxEval
-    else:
-        minEval = +infinity
-        for each child of state:
-            eval = minimax(child, depth - 1, true)
-            mixEval = min(minEval, eval)
-        return minEval
-    '''  
-    
-    return
-
