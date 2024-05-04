076601566e8193adc0a2255c1f43fecddb43ec51
Eden Xu <130119691+FlyingPufferFish@users.noreply.github.com>
2024-05-04 12:59:10 +0000

    Add properties and setters to TreeNode

--- monte/tree.py
+++ monte/tree.py
@@ -1,5 +1,8 @@
 from referee.game.constants import *
 from board import Board
+from math import log2
+
+UCB1_C = 2      # constant for balancing exploration & exploitation in UCB1
 
 class TreeNode:
 
@@ -12,17 +15,24 @@ class TreeNode:
         board: Board = Board(),
         score: int = 0,
         times_visited: int = 0,
-        children=None,
-        parent=None,
+        children = None,
+        parent = None,
     ):
         self.board = board
         self._score = score
         self._times_visited = times_visited
         self.children = []
+        self.parent = parent
+
         if children is not None:
             for child in children:
                 self.add_child(child)
     
+    def is_root(self):
+        if self.parent is None:
+            return True
+        return False
+
     def is_leaf(self):
         if len(self.children) == 0:
             return True
@@ -42,3 +52,17 @@ class TreeNode:
     @score.setter
     def score(self, value):
         self._score = value
+
+    @property
+    def times_visited(self):
+        return self._times_visited
+    
+    @times_visited.setter
+    def times_visited(self, value):
+        self._times_visited = value
+
+    def UCB1(self):
+        return 
+
+    def backpropagation(self):
+        pass
