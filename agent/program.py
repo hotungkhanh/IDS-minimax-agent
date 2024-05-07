06dda17f7bd77239b2985a27b6f350533ef7ad99
tungkhanhh <tungkhanh.ho@student.unimelb.edu.au>
2024-05-07 13:41:55 +0000

    Change minimax_ab() to use maximising Boolean

--- agent/program.py
+++ agent/program.py
@@ -38,7 +38,7 @@ class Agent:
         # print("board in action()")
         # print(self.board.render(True, True))
         # print("done")
-        eval, child = minimax_ab(self.board, 3, -(math.inf), math.inf, self._color)
+        eval, child = minimax_ab(self.board, 2, -(math.inf), math.inf, True)
         # action should never be None
         action = child.last_piece    
 
@@ -68,94 +68,6 @@ class Agent:
         # to update your agent's internal game state representation.
         print(f"Testing: {color} played PLACE action: {c1}, {c2}, {c3}, {c4}")
 
-    
-def generate_moves(board: Board, color: PlayerColor):
-    '''
-    Returns a list of boards with new valid moves that can be made 
-    '''
-    moves = set()          # needs to be a list of states here 
-    # no piece of player colour on board
-    # FOR TESTING PURPOSES: remove randomness of first step
-
-    if color == PlayerColor.RED:
-        my_cells = board.red_cells
-    else:
-        my_cells = board.blue_cells
-
-    if len(my_cells) == 0:
-        if len(board.red_cells) == 0 and len(board.blue_cells) == 0:            
-            action = PlaceAction(
-                    Coord(3, 3), 
-                    Coord(3, 4), 
-                    Coord(4, 3), 
-                    Coord(4, 4)
-                )
-
-            # place piece on board
-            new_board = Board(board.red_cells.copy(), board.blue_cells.copy(), board._turn_color, action, board.turn_count)
-            new_board.apply_action(action)
-            moves.add(new_board)
-            return moves
-        
-        empty_coords = [
-            Coord(r, c)
-            for r in range(BOARD_N)
-            for c in range(BOARD_N)
-            if ((Coord(r, c) not in board.red_cells) or (Coord(r, c) not in board.blue_cells))
-        ]
-        for cell in empty_coords:
-            piece_combinations = board.generate_piece_combinations(cell)
-
-            # code for random piece 
-            for piece in piece_combinations:
-                c1, c2, c3, c4 = piece
-                action = PlaceAction(c1, c2, c3, c4)
-
-                new_board = Board(board.red_cells.copy(), board.blue_cells.copy(), board._turn_color, action, board.turn_count)
-                new_board.apply_action(action)
-                moves.add(new_board)
-
-            # code for less random piece
-            # for i in range(20):
-            #     if len(piece_combinations) == 0:
-            #         # print('ooops')
-            #         break
-            #     piece = random.choice(list(piece_combinations))
-            #     piece_combinations.remove(piece)
-            #     c1, c2, c3, c4 = piece
-            #     action = PlaceAction(c1, c2, c3, c4)
-
-            #     # place piece on board
-            #     new_board = Board(board.red_cells.copy(), board.blue_cells.copy(), board._turn_color, action, board.turn_count)
-            #     new_board.apply_action(action)
-            #     moves.add(new_board)
-
-        return moves
-    
-    # board has 1+ piece of player colour
-    # FOR TESTING PURPOSES: reduce randomness
-    else:
-        for cell in my_cells:
-            # if cell does not have empty neighbours
-            #   continue
-
-            piece_combinations = board.generate_piece_combinations(cell)
-
-            # code for all pieces
-            for piece in piece_combinations:
-                # if len(piece_combinations) == 0:
-                #     # print('ooops')
-                #     break
-                # piece = random.choice(list(piece_combinations))
-                # piece_combinations.remove(piece)
-                c1, c2, c3, c4 = piece
-                action = PlaceAction(c1, c2, c3, c4)
-
-                # place piece on board
-                new_board = Board(board.red_cells.copy(), board.blue_cells.copy(), board._turn_color, action, board.turn_count)
-                new_board.apply_action(action)
-                moves.add(new_board)
-        return moves
 
 def empty_neighbours(board: Board, coord: Coord) -> list[Coord]:
     neighbours = [coord.down(), coord.up(), coord.left(), coord.right()]
@@ -166,62 +78,18 @@ def empty_neighbours(board: Board, coord: Coord) -> list[Coord]:
     
     return output
 
-
-def minimax(board: Board, depth: int, color: PlayerColor) -> tuple[int, Board]:
-    ''' 
-    recursive pseudocode:
-
-    if depth == 0 or game over in state then:
-        return static eval of state
-
-    if maximisingPlayer then:
-        maxEval = -infinity
-        for each child of state:
-            eval = minimax(child, depth - 1, false)
-            maxEval = max(maxEval, eval)
-        return maxEval
-    else:
-        minEval = +infinity
-        for each child of state:
-            eval = minimax(child, depth - 1, true)
-            minEval = min(minEval, eval)
-        return minEval
-    '''  
+def minimax_ab(board: Board, depth: int, alpha, beta, maximising: bool) -> tuple[int, Board]:
 
     if depth == 0 or board.game_over:
         return (eval(board), None)
-    if color == PlayerColor.RED:
-        best_child = None
-        maxEval = -(math.inf)
-        children = generate_moves(board, color)
-        for child in children:
-            val = minimax(child, depth - 1, PlayerColor.BLUE)
-            if val[0] > maxEval:
-                maxEval = val[0]
-                best_child = child
-        return (maxEval, best_child)
-    else:
-        best_child = None
-        minEval = math.inf
-        children = generate_moves(board, color)
-        for child in children:
-            val = minimax(child, depth - 1, PlayerColor.RED)
-            if val[0] < minEval:
-                minEval = val[0]
-                best_child = child
-        return (minEval, best_child)
     
-
-def minimax_ab(board: Board, depth: int, alpha, beta, color: PlayerColor) -> tuple[int, Board]:
-
-    if depth == 0 or board.game_over:
-        return (eval(board), None)
-    if color == PlayerColor.RED:
+    if maximising:
         best_child = None
         maxEval = -(math.inf)
-        children = generate_moves(board, color)
+        children = board.generate_all_children()
+
         for child in children:
-            val = minimax_ab(child, depth - 1, alpha, beta, PlayerColor.BLUE)
+            val = minimax_ab(child, depth - 1, alpha, beta, False)
             if val[0] > maxEval:
                 maxEval = val[0]
                 best_child = child
@@ -229,22 +97,40 @@ def minimax_ab(board: Board, depth: int, alpha, beta, color: PlayerColor) -> tup
             if beta <= alpha:
                 break
         return (maxEval, best_child)
+    
     else:
         best_child = None
         minEval = math.inf
-        children = generate_moves(board, color)
+        children = board.generate_all_children()
+
         for child in children:
-            val = minimax_ab(child, depth - 1, alpha, beta, PlayerColor.RED)
+            val = minimax_ab(child, depth - 1, alpha, beta, True)
             if val[0] < minEval:
                 minEval = val[0]
                 best_child = child
-            beta = min(minEval, val[0])
+            beta = min(beta, val[0])
             if beta <= alpha:
                 break
         return (minEval, best_child)
 
 
 def eval(board: Board):
-    blue_count = len(board.blue_cells)
-    red_count = len(board.red_cells)
-    return red_count - blue_count
+    if board._turn_color == PlayerColor.RED:
+        my_cells = board.red_cells
+        opponent_cells = board.blue_cells
+    else:
+        my_cells = board.blue_cells
+        opponent_cells = board.red_cells
+
+    line_with_many_cells = 0
+    for r in range(BOARD_N):
+        num = len([cell for cell in my_cells if cell.r == r])
+        if num >= 6:
+            line_with_many_cells += 1
+    
+    for c in range(BOARD_N):
+        num = len([cell for cell in my_cells if cell.c == c])
+        if num >= 6:
+            line_with_many_cells += 1
+
+    return len(my_cells) - len(opponent_cells) - line_with_many_cells
