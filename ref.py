801bc1b7822ecfca694df291b706dd396384b26d
Eden Xu <130119691+FlyingPufferFish@users.noreply.github.com>
2024-05-09 03:08:09 +0000

    Update eval() to consider #cells/line occupied, ref.py working with minimax

--- ref.py
+++ ref.py
@@ -5,6 +5,7 @@ import random, math, copy
 from datetime import datetime, timedelta
 
 # contestants
+from agent.program import Agent as minimax
 from monte.program import Agent as monte
 from randy.program import Agent as randy
 
@@ -15,7 +16,7 @@ MAX_TURNS = 150
 BOARD_N = 11
 
 def play():
-    agent1 = monte(PlayerColor.RED)
+    agent1 = minimax(PlayerColor.RED)
     agent2 = randy(PlayerColor.BLUE)
     players = {
         PlayerColor.RED: agent1,
@@ -30,14 +31,13 @@ def play():
         print()
         print(game_board._turn_color, "playing turn", game_board.turn_count + 1)
 
-        action: PlaceAction = players[game_board._turn_color].action()
-        turn_colour: PlayerColor = game_board._turn_color
-        print("variable turn_colour:", turn_colour)
-        print("RED and BLUE board before applying action")
-        # print(agent1.board.render())
-        # print(agent2.board.render())
-
         try:
+            action: PlaceAction = players[game_board._turn_color].action()
+            turn_colour: PlayerColor = game_board._turn_color
+            print("variable turn_colour:", turn_colour)
+            print("RED and BLUE board before applying action")
+            # print(agent1.board.render())
+            # print(agent2.board.render())`
             game_board.apply_action(action)
             print("game board turn colour:", game_board._turn_color)
             print(game_board.render())
